1e785627051d8546f01728b51dcd57a0f17b4834
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-07 16:03:24 +0200

    m

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,7 +22,7 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 if ($request->expectsJson()) {
-                    return response()->json([], 200);
+                    return response()->json(["erreur" => 'utilisateur déjà authentifié'], 200);
                 }
                 return redirect(RouteServiceProvider::HOME);
             }
